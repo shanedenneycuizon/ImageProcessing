534852187efb1c18725adaa24cd509e287952e35
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2022-12-08 19:01:06 +0800

    Change codes that uses libraries

--- IS_DIP_Cuizon/IS_DIP_Cuizon/Form1.Designer.cs
+++ IS_DIP_Cuizon/IS_DIP_Cuizon/Form1.Designer.cs
@@ -38,13 +38,21 @@
             this.colorInversionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.histogramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.imageButton = new System.Windows.Forms.Button();
+            this.bgButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -55,7 +63,7 @@
             this.dIPToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(852, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1177, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -89,7 +97,8 @@
             this.greyscaleToolStripMenuItem,
             this.colorInversionToolStripMenuItem,
             this.histogramToolStripMenuItem,
-            this.sepiaToolStripMenuItem});
+            this.sepiaToolStripMenuItem,
+            this.subtractToolStripMenuItem});
             this.dIPToolStripMenuItem.Name = "dIPToolStripMenuItem";
             this.dIPToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
             this.dIPToolStripMenuItem.Text = "DIP";
@@ -97,52 +106,59 @@
             // basicCopyToolStripMenuItem
             // 
             this.basicCopyToolStripMenuItem.Name = "basicCopyToolStripMenuItem";
-            this.basicCopyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.basicCopyToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.basicCopyToolStripMenuItem.Text = "Basic Copy";
             this.basicCopyToolStripMenuItem.Click += new System.EventHandler(this.basicCopyToolStripMenuItem_Click);
             // 
             // greyscaleToolStripMenuItem
             // 
             this.greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
-            this.greyscaleToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.greyscaleToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.greyscaleToolStripMenuItem.Text = "Greyscale";
             this.greyscaleToolStripMenuItem.Click += new System.EventHandler(this.greyscaleToolStripMenuItem_Click);
             // 
             // colorInversionToolStripMenuItem
             // 
             this.colorInversionToolStripMenuItem.Name = "colorInversionToolStripMenuItem";
-            this.colorInversionToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.colorInversionToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.colorInversionToolStripMenuItem.Text = "Color Inversion";
             this.colorInversionToolStripMenuItem.Click += new System.EventHandler(this.colorInversionToolStripMenuItem_Click);
             // 
             // histogramToolStripMenuItem
             // 
             this.histogramToolStripMenuItem.Name = "histogramToolStripMenuItem";
-            this.histogramToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.histogramToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.histogramToolStripMenuItem.Text = "Histogram";
             this.histogramToolStripMenuItem.Click += new System.EventHandler(this.histogramToolStripMenuItem_Click);
             // 
             // sepiaToolStripMenuItem
             // 
             this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
-            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
             this.sepiaToolStripMenuItem.Text = "Sepia";
             this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
+            // 
+            // subtractToolStripMenuItem
+            // 
+            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
+            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
+            this.subtractToolStripMenuItem.Text = "Subtract";
+            this.subtractToolStripMenuItem.Click += new System.EventHandler(this.subtractToolStripMenuItem_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(34, 52);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(370, 370);
+            this.pictureBox1.Size = new System.Drawing.Size(352, 240);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(443, 52);
+            this.pictureBox2.Location = new System.Drawing.Point(417, 52);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(370, 370);
+            this.pictureBox2.Size = new System.Drawing.Size(352, 240);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -152,21 +168,74 @@
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(799, 52);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(352, 240);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // imageButton
+            // 
+            this.imageButton.Location = new System.Drawing.Point(141, 309);
+            this.imageButton.Name = "imageButton";
+            this.imageButton.Size = new System.Drawing.Size(106, 31);
+            this.imageButton.TabIndex = 4;
+            this.imageButton.Text = "Load Image";
+            this.imageButton.UseVisualStyleBackColor = true;
+            this.imageButton.Click += new System.EventHandler(this.imageButton_Click);
+            // 
+            // bgButton
+            // 
+            this.bgButton.Location = new System.Drawing.Point(484, 309);
+            this.bgButton.Name = "bgButton";
+            this.bgButton.Size = new System.Drawing.Size(209, 31);
+            this.bgButton.TabIndex = 5;
+            this.bgButton.Text = "Load Background Image";
+            this.bgButton.UseVisualStyleBackColor = true;
+            this.bgButton.Click += new System.EventHandler(this.bgButton_Click);
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(940, 309);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(146, 23);
+            this.subtractButton.TabIndex = 6;
+            this.subtractButton.Text = "Subtract";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            this.subtractButton.Click += new System.EventHandler(this.subtractButton_Click);
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            this.openFileDialog3.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog3_FileOk);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(852, 503);
+            this.ClientSize = new System.Drawing.Size(1177, 394);
+            this.Controls.Add(this.subtractButton);
+            this.Controls.Add(this.bgButton);
+            this.Controls.Add(this.imageButton);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cuizon_DIP";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,6 +257,13 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button imageButton;
+        private System.Windows.Forms.Button bgButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
     }
 }
 
--- IS_DIP_Cuizon/IS_DIP_Cuizon/Form1.cs
+++ IS_DIP_Cuizon/IS_DIP_Cuizon/Form1.cs
@@ -14,10 +14,23 @@ namespace IS_DIP_Cuizon
 {
     public partial class Form1 : Form
     {
-        private Bitmap loaded, processed;
+        private Bitmap loaded, imageA, imageB, processed,resultImage;
         public Form1()
         {
             InitializeComponent();
+            imageButton.Hide();
+            bgButton.Hide();
+            subtractButton.Hide();
+
+        }
+
+        private void Reset()
+        {
+            imageButton.Hide();
+            bgButton.Hide();
+            subtractButton.Hide();
+            pictureBox1.Image = null;
+            pictureBox2.Image=null;
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
@@ -30,25 +43,56 @@ namespace IS_DIP_Cuizon
             loaded = new Bitmap(openFileDialog1.FileName);
             pictureBox1.Image= loaded;
         }
+        
 
         private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ImageProcess.copyImage(ref loaded, ref processed);
+
+            processed = new Bitmap(loaded.Width, loaded.Height);
+
+            
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color data = loaded.GetPixel(x, y);
+                    int grey = (data.R+data.G+data.B)/3;
+                    processed.SetPixel(x, y, Color.FromArgb(grey,grey,grey));
+                }
+            }
             pictureBox2.Image = processed;
-            BitmapFilter.GrayScale(processed);
         }
 
         private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ImageProcess.copyImage(ref loaded, ref processed);
+            processed = new Bitmap(loaded.Width, loaded.Height);
+
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color data = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, data);
+                }
+
+            }
             pictureBox2.Image = processed;
         }
 
         private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ImageProcess.copyImage(ref loaded, ref processed);
+            processed = new Bitmap(loaded.Width, loaded.Height);
+
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color data = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, Color.FromArgb(255-data.R,255-data.G,255-data.B));
+                }
+
+            }
             pictureBox2.Image = processed;
-            BitmapFilter.Invert(processed);
         }
 
         private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
@@ -74,12 +118,66 @@ namespace IS_DIP_Cuizon
 
         private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ImageProcess.Histogram(ref loaded, ref processed);
-            pictureBox2.Image = processed;
+            
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            imageB = new Bitmap(openFileDialog2.FileName);
+            pictureBox1.Image= imageB;
+        }
+
+        private void openFileDialog3_FileOk(object sender, CancelEventArgs e)
+        {
+            imageA = new Bitmap(openFileDialog3.FileName);
+            pictureBox2.Image= imageA;
+        }
+
+        private void bgButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog3.ShowDialog();
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(imageB.Width, imageB.Height);
+            Color mygreen = Color.FromArgb(0, 255, 0);
+            int greygreen = (mygreen.R + mygreen.G + mygreen.B)/3;
+            int threshold = 5;
+
+            for (int x = 0; x<imageB.Width;x++)
+            {
+                for (int y = 0; y <imageB.Height; y++)
+                {
+                    Color pixel = imageB.GetPixel(x, y);
+                    Color backpixel = imageA.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    int subtractvalue = Math.Abs(grey - greygreen);
+                    if(subtractvalue < threshold)
+                        processed.SetPixel(x, y, backpixel);
+                    else
+                        processed.SetPixel(x, y, pixel);
+                }
+            }
+            pictureBox3.Image = processed;
+        }
+
+        private void imageButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void subtractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reset();
+            imageButton.Show();
+            bgButton.Show();
+            subtractButton.Show();
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            Reset();
             openFileDialog1.ShowDialog();
         }
     }
